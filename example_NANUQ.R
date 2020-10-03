68d4dac06e29131ffca07f1c416bfcfe9f5c8c3e
SethMusker <sethmusker@gmail.com>
2020-10-03 22:48:47 +0200

    Create example_NANUQ.R

---
+++ example_NANUQ.R
@@ -0,0 +1,15 @@
+library(MSCquartets)
+library(future.apply)
+plan(multiprocess,gc=TRUE)
+
+source("Parallel_NANUQ.R")
+
+taxa<-read.table("mytaxa.txt") # taxon names in one column w/out header
+taxa<-as.vector(taxa$V1)
+
+all_nanuq<-Parallel_NANUQ(genedata="genetrees.tre",
+                 taxanames = taxa,
+                 outfile = "genetrees_NANUQ.nex",
+		             RAM_Gigs=5)
+
+
